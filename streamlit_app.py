3c81c04aa4c71203cb3a44c35c01c011bf50e215
TEXTMEDATA <122851239+TEXTMEDATA@users.noreply.github.com>
2023-01-17 10:08:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents new healthy Diner')
-streamlit.title('this is my first code')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
