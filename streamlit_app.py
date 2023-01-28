1639f8da32f2a8c2b3a61c2ab9f48baa0d4d13fe
TEXTMEDATA <122851239+TEXTMEDATA@users.noreply.github.com>
2023-01-28 19:45:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
-
+import requests
+import snowflake.connector
+from urllib.error import URLError
 streamlit.title('My Parents new healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -23,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 # write your own comment -what does the next line do? 
